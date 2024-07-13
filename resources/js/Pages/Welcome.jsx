134b455a791b534dbbb2f48204878e60d4d306fd
Brian James <brianjamesdlcz@gmail.com>
2024-07-13 21:30:13 +0800

    check branch

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -18,9 +18,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
               <Link
                 href={route('login')}
                 className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-              >
-                Log in
-              </Link>
+              ></Link>
 
               <Link
                 href={route('register')}
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,15 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/Admin/Login', function () {
+    return Inertia::render('Login', [
+        'canLogin' => Route::has('Login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
