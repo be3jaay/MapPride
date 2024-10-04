40d0ce5d76ce7cf2ff68b511913d886dcd2e1733
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-10-04 22:42:26 +0800

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/js/app.jsx'],
       refresh: true,
     }),
   ],
-  build: {
-    manifest: true,
-    outDir: 'public/build',
-  },
 });
