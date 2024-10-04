c41cd3cf1f77340b8da3307b359cbb8862b62577
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-10-05 00:24:45 +0800

    Fix composer configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
   plugins: [
+    react(),
     laravel({
       input: ['resources/js/app.jsx'],
       refresh: true,
