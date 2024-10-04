43ba9b0e9200f595e4f8f3286284c011209dee4a
Brian James (BJ) <brianjamesdlcz@gmail.com>
2024-10-05 00:17:23 +0800

    Fix composer configuration

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: '/build/',
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/js/app.jsx'],
       refresh: true,
     }),
   ],
-  build: {
-    manifest: true,
-    outDir: 'public/build',
-  },
 });
